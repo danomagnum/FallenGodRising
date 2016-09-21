4f3fa63a690051d0cf19ce6a08268acbefea9478
dano <dano@5704632f-9846-42e2-9197-bb39f3c2fcad>
2016-09-21 02:55:38 +0000

    getting close to a functing battle system.  The enemy can swap pokemon and choose attacks.
    Still need to put those kind of choices in for the user

--- battle.py
+++ battle.py
@@ -8,30 +8,57 @@ SWITCH = 1
 ITEM = 2
 
 class Random_AI(object):
-	def __init__(self, name='AI', items=None):
+	def __init__(self, combatants, money=0, name='AI', items=None, defeated_text='Oh Snap! I lost!'):
 		self.name = name
+		self.combatants = combatants
+		self.combatant = combatants[0]
+		self.defeated_text = defeated_text
+
+		if money == 0:
+			self.money = int(sum([c.exp_value for c in combatants]) / 10)
+		else:
+			self.money = money
+
 		if items is not None:
 			self.items = items
 		else:
 			self.items = []
 
-	def attack(self, user, enemy):
-		move = random.choice(user.moves)
-		target = [user, enemy][move.default_target]
+	def attack(self, enemy):
+		move = random.choice(self.combatant.moves)
+		target = [self.combatant, enemy][move.default_target]
 		return [move, target]
 
-	def change(self, valid_list, enemy):
-		return random.choice(valid_list)
+	def change(self, enemy):
+		standby = self.get_standby()
+		available = self.get_available()
+		if standby:
+			self.combatant = random.choice(standby)
+			return self.combatant
+		elif available:
+			self.combatant = random.choice(available)
+			return self.combatant
+		else:
+			return None
 	
-	def action(self, user, enemy):
-		return random.choice([ATTACK, SWITCH])
+	def action(self, enemy):
+		if self.get_standby():
+			return random.choice([ATTACK, SWITCH])
+		else:
+			return ATTACK
 	
-def Battle(users, enemies, display, AI):
-	user = users[0]
-	enemy = enemies[0]
-	valid_users = [ combatant for combatant in users if combatant.hp > 0 ]
-	valid_enemies = [ combatant for combatant in enemies if combatant.hp > 0 ]
+	def get_available(self):
+		return [ combatant for combatant in self.combatants if combatant.hp > 0 ] 
+	
+	def get_standby(self):
+		return [ combatant for combatant in self.combatants if (combatant.hp > 0) and combatant != self.combatant ] 
+	
+def Battle(user_combatants, enemy_ai, display):
+	user = user_combatants[0]
+	valid_users = [ combatant for combatant in user_combatants if combatant.hp > 0 ]
+	valid_enemies = enemy_ai.get_available()
 	all_combatants = valid_users + valid_enemies
+
 	battle_continue = True
 	last_attack = 0
 
@@ -42,8 +69,8 @@ def Battle(users, enemies, display, AI):
 	display.refresh_combatant()
 
 	while battle_continue:
-		valid_users = [ combatant for combatant in users if combatant.hp > 0 ]
-		valid_enemies = [ combatant for combatant in enemies if combatant.hp > 0 ]
+		valid_users = [ combatant for combatant in user_combatants if combatant.hp > 0 ]
+		valid_enemies = enemy_ai.get_available()
 		all_combatants = valid_users + valid_enemies
 
 		#make sure all pre-turn status take effect
@@ -52,62 +79,79 @@ def Battle(users, enemies, display, AI):
 				status.pre_turn(combatant)
 		display.refresh_combatant()
 
+		#have the user select a move
 		user_move = display.menu(user.moves, cols=2, selected=last_attack)
 		for move_id in xrange(len(user.moves)):
 			if user.moves[move_id] == user_move:
 				last_attack = move_id
+		user_is_attacking = True
 
-		user_target = [user, enemy][user_move.default_target]
-
-		enemy_move, enemy_target = AI.attack(enemy, user)
-
-		if user.speed >= enemy.speed:
-			for status in user.status:
-				status.pre_attack(user)
+		#have the enemy select a move and target
+		enemy_decision = enemy_ai.action(user)
+		if enemy_decision == ATTACK:
+			enemy_move, enemy_target = enemy_ai.attack(user)
+			enemy_is_attacking = True
+		elif enemy_decision == SWITCH:
+			enemy_ai.change(user)
+			enemy_move = None
+			enemy_target = None
+			enemy_is_attacking = False
+			display.enemy = enemy_ai.combatant
+			print enemy_ai.name, 'switched to', enemy_ai.combatant.name
 			display.refresh_combatant()
 
-			user_move.attack(user, user_target)
-			display.refresh_combatant()
+		#select the target of the user's move. Have to do this after the enemy has
+		#gone so that you target their new combatant if they switch or something
+		if user_is_attacking:
+			user_target = [user, enemy_ai.combatant][user_move.default_target]
 
-			for status in user.status:
-				status.post_attack(user)
-
-			display.refresh_combatant()
-			if enemy.hp > 0:
-
-				for status in enemy.status:
-					status.pre_attack(enemy)
-				display.refresh_combatant()
-
-				enemy_move.attack(enemy, enemy_target)
-				display.refresh_combatant()
-
-				for status in enemy.status:
-					status.post_attack(enemy)
-
-				display.refresh_combatant()
+		#decide who should attack first
+		if user.speed >= enemy_ai.combatant.speed:
+			first = user
+			first_target = user_target
+			first_move = user_move
+			first_is_attacking = user_is_attacking
+			second = enemy_ai.combatant
+			second_target = enemy_target
+			second_move = enemy_move
+			second_is_attacking = enemy_is_attacking
 		else:
-			for status in enemy.status:
-				status.pre_attack(enemy)
-			display.refresh_combatant()
-			enemy_move.attack(enemy, enemy_target)
-			display.refresh_combatant()
-			for status in enemy.status:
-				status.post_attack(enemy)
-			display.refresh_combatant()
-			display.refresh_combatant()
-			display.show_messages()
-			if user.hp > 0:
-				for status in user.status:
-					status.pre_attack(user)
-				display.refresh_combatant()
-				user_move.attack(user, user_target)
-				display.refresh_combatant()
-				for status in user.status:
-					status.post_attack(user)
-				display.refresh_combatant()
+			first = enemy_ai.combatant
+			first_target = enemy_target
+			first_move = enemy_move
+			first_is_attacking = enemy_is_attacking
+			second = user
+			second_target = user_target
+			second_move = user_move
+			second_is_attacking = user_is_attacking
 
+		#first attack
+		if first_is_attacking:
+			for status in first.status:
+				status.pre_attack(first)
+			display.refresh_combatant()
 
+			first_move.attack(first, first_target)
+			display.refresh_combatant()
+
+			for status in first.status:
+				status.post_attack(first)
+			display.refresh_combatant()
+
+		#second attack, assuming they did not die
+		if second_is_attacking and second.hp > 0:
+			for status in second.status:
+				status.pre_attack(second)
+			display.refresh_combatant()
+
+			second_move.attack(second, second_target)
+			display.refresh_combatant()
+
+			for status in second.status:
+				status.post_attack(second)
+
+			display.refresh_combatant()
+	
 		# make sure all post-turn status take place
 		for combatant in all_combatants:
 			for status in combatant.status:
@@ -116,17 +160,15 @@ def Battle(users, enemies, display, AI):
 
 
 		# check for any deaths
-		if enemy.hp == 0:
-			print enemy.name, 'fainted'
-			exp = enemy.exp_value
+		if enemy_ai.combatant.hp == 0:
+			print enemy_ai.combatant.name, 'fainted'
+			exp = enemy_ai.combatant.exp_value
 			print user.name, 'gained', int(exp), 'xp. ', ((user.level+1) ** 3) - user.exp, 'to go'
 			user.exp += exp
 
-			valid_enemies = [ combatant for combatant in enemies if combatant.hp > 0 ]
-			if valid_enemies:
-				enemy = AI.change(valid_enemies, user)
-				print AI.name, 'sent out', enemy.name
-				display.enemy = enemy
+			if enemy_ai.change(user) is not None:
+				print enemy_ai.name, 'sent out', enemy_ai.combatant.name
+				display.enemy = enemy_ai.combatant
 				display.refresh_combatant()
 			else:
 				battle_continue = False
@@ -138,10 +180,12 @@ def Battle(users, enemies, display, AI):
 
 		display.refresh_combatant()
 
-		
-		display.screen.refresh()
 		time.sleep(1.0 / 60.0)
 	for status in user.status:
 		status.post_battle(user)
+	
+	print 'Got', enemy_ai.money, 'for winning'
+	print enemy_ai.name, ':', enemy_ai.defeated_text
 
+	display.show_messages()
 
