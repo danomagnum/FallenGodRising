842e957dba29a14ee7d4e8e8c7eee65ba9bd64fa
dano <dano@5704632f-9846-42e2-9197-bb39f3c2fcad>
2017-11-30 03:16:04 +0000

    worked through a number of things that needed changed

--- effects.py
+++ effects.py
@@ -7,9 +7,25 @@ class Strength2x(Status):
 	def physical_strength(self, initial):
 		return initial * 2
 
+class Strength_Mult(Status):
+	def __init__(self, multiplier):
+		self.multiplier = multiplier
+		self.name = 'Strength ' + str(self.multiplier) + '%'
+	def physical_strength(self, initial):
+		return initial * self.multiplier
+
+
+
 class Poison_Minor(Status):
 	def __init__(self):
 		self.name = 'Minor Poison'
 	def pre_turn(self, effected):
 		print effected.name, 'was effected by', self.name
 		effected.hp -= max(1, effected.hp / 10)
+
+class Poison_Major(Status):
+	def __init__(self):
+		self.name = 'Major Poison'
+	def pre_turn(self, effected):
+		print effected.name, 'was effected by', self.name
+		effected.hp -= max(1, effected.hp / 6)
--- moves.py
+++ moves.py
@@ -2,17 +2,96 @@ import main
 import random
 import elements
 import effects
+from utility import clamp, scale
 
-def MoveGen(name, elementlist=None, effect_list=None, power=10.0, mp=10.0, cost=1, accuracy=0.9, target=main.ENEMY):
-	if elementlist is None:
-		elementlist = [elements.Normal]
-	if effect_list is None:
-		effect_list = [] # effects should be a list of tuples (effect, chance as 0 < real < 1)
-	return lambda: main.Move(name, elementlist, accuracy, power, mp, effect_list, target)
 
-Pound = MoveGen('Pound')
-Slam = MoveGen('Slam', power=20)
+class Pound(main.Move):
+	def __init__(self):
+		self.name = 'Pound'
+		self.mp = 10.0
+		self.max_mp = 10.0
+		self.accuracy = 0.9
+		self.power = 10.0
+		self.elements = [elements.Normal]
+		self.uses = 0
+		self.default_target = main.ENEMY
 
-Buff = MoveGen('Buff', effect_list=[(effects.Strength2x(), 1)], accuracy = 1 , power=0, target=main.SELF)
 
-Poison = MoveGen('Poison', effect_list=[(effects.Poison_Minor(), 1)], power=0)
+class Slam(main.Move):
+	def __init__(self):
+		self.name = 'Slam'
+		self.mp = 10.0
+		self.max_mp = 10.0
+		self.accuracy = 0.9
+		self.power = 20.0
+		self.elements = [elements.Normal]
+		self.uses = 0
+		self.default_target = main.ENEMY
+
+
+
+class Spray(main.Move):
+	def __init__(self):
+		self.name = 'Spray'
+		self.mp = 10.0
+		self.max_mp = 10.0
+		self.accuracy = 0.9
+		self.power = 20.0
+		self.elements = [elements.Water]
+		self.uses = 0
+		self.default_target = main.ENEMY
+
+
+
+class Buff(main.Move):
+	def __init__(self):
+		self.name = 'Buff'
+		self.mp = 10.0
+		self.max_mp = 10.0
+		self.accuracy = 1
+		self.power = 0
+		self.elements = [elements.Normal]
+		self.uses = 0
+		self.default_target = main.SELF
+
+	def effect(self, target):
+		target.status.append(effects.Strength_Mult(1.15))
+
+
+class Debuff(main.Move):
+	def __init__(self):
+		self.name = 'Debuff'
+		self.mp = 10.0
+		self.max_mp = 10.0
+		self.accuracy = 1
+		self.power = 0
+		self.elements = [elements.Normal]
+		self.uses = 0
+		self.default_target = main.ENEMY
+
+	def effect(self, target):
+		target.status.append(effects.Strength_Mult(0.85))
+
+class Poison(main.Move):
+	def __init__(self):
+		self.name = 'Poison'
+		self.mp = 10.0
+		self.max_mp = 10.0
+		self.accuracy = 0.9
+		self.power = 2.0
+		self.elements = [elements.Normal]
+		self.uses = 0
+		self.default_target = main.ENEMY
+
+
+	def effect(self, target):
+		randval = random.random()
+		# for starters, poison minorly 20% of the time.  Once the move is more used, poison minorly 95%
+		minor_range = scale(self.uses, 0, 1000, 0.2, 0.95)
+		# for starters, poison majorly 5% of the time.  Once the move is more used, poison major 50%
+		major_range = scale(self.uses, 0, 1000, 0.05, 0.5)
+		if randval < major_range:
+			target.status.append(effects.Poison_Major())
+		elif randval < minor_range:
+			target.status.append(effects.Poison_Minor())
+
