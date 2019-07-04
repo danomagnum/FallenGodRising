b938f4c1bb06cdd5484b0585aa2f3b40d458ad14
dano <266759+danomagnum@users.noreply.github.com>
2019-07-04 18:46:37 +0000

    got a new game character select working

--- characters.py
+++ characters.py
@@ -4,14 +4,24 @@ import items
 import elements
 import moves
 
+def gen_testuser():
+	sys.stdout.silent = True
+	battleuser = Fighter('Fighter Joe', 55)
+	battleuser2 = Wizard('MiniMage', 50)
+
+	user = Entity('playercharacter', combatants=[battleuser, battleuser2], item_list=[items.Potion(), items.Potion(), items.Booster(), items.HealAll()], char='@',is_player=True)
+
+	sys.stdout.silent = False
+	return user
+
 
 class Fighter(Character):
 	def config(self):
-		self.moves = [moves.Pound(), moves.Slam(), moves.Buff()]
-		self.elements = [elements.Normal]
+		self.moves = []
+		self.elements = []
 		self.status = []
 		self.coefficients = (1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
-		self.base_physical_strength = 10
+		self.base_physical_strength = 10 
 		self.base_physical_defense = 10
 		self.base_special_strength = 10
 		self.base_special_defense = 10
@@ -19,13 +29,14 @@ class Fighter(Character):
 		self.base_hp = 10
 		self.base_luck = 10
 
-class Page(Character):
+
+class Wizard(Character):
 	def config(self):
-		self.moves = [moves.Pound(), moves.Poison(), moves.Spray()]
-		self.elements = [elements.Fire]
+		self.moves = []
+		self.elements = []
 		self.status = []
 		self.coefficients = (1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
-		self.base_physical_strength = 8
+		self.base_physical_strength = 8 
 		self.base_physical_defense = 8
 		self.base_special_strength = 12
 		self.base_special_defense = 12
@@ -35,8 +46,8 @@ class Page(Character):
 
 class Cleric(Character):
 	def config(self):
-		self.moves = [moves.Pound(), moves.Heal()]
-		self.elements = [elements.Water]
+		self.moves = []
+		self.elements = []
 		self.status = []
 		self.coefficients = (1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
 		self.base_physical_strength = 8
@@ -47,14 +58,129 @@ class Cleric(Character):
 		self.base_hp = 12
 		self.base_luck = 10
 
-def gen_testuser():
-	sys.stdout.silent = True
-	battleuser = Fighter('Fighter Joe', 55)
-	battleuser2 = Page('MiniMage', 50)
+class Knight(Character):
+	def config(self):
+		self.moves = []
+		self.elements = []
+		self.status = []
+		self.coefficients = (1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+		self.base_physical_strength = 12
+		self.base_physical_defense = 12
+		self.base_special_strength = 8
+		self.base_special_defense = 8
+		self.base_speed = 10
+		self.base_hp = 10
+		self.base_luck = 10
 
-	user = Entity('playercharacter', combatants=[battleuser, battleuser2], item_list=[items.Potion(), items.Potion(), items.Booster(), items.HealAll()], char='@',is_player=True)
+class Paladin(Character):
+	def config(self):
+		self.moves = []
+		self.elements = []
+		self.status = []
+		self.coefficients = (1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+		self.base_physical_strength = 12
+		self.base_physical_defense = 10
+		self.base_special_strength = 6
+		self.base_special_defense = 12
+		self.base_speed = 10
+		self.base_hp = 10
+		self.base_luck = 10
 
-	sys.stdout.silent = False
-	return user
+class Rogue(Character):
+	def config(self):
+		self.moves = []
+		self.elements = []
+		self.status = []
+		self.coefficients = (1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+		self.base_physical_strength = 10
+		self.base_physical_defense = 10
+		self.base_special_strength = 8
+		self.base_special_defense = 10
+		self.base_speed = 12
+		self.base_hp = 8
+		self.base_luck = 12
 
+class Dragoon(Character):
+	def config(self):
+		self.moves = []
+		self.elements = []
+		self.status = []
+		self.coefficients = (1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+		self.base_physical_strength = 12
+		self.base_physical_defense = 8
+		self.base_special_strength = 10
+		self.base_special_defense = 8
+		self.base_speed = 12
+		self.base_hp = 10
+		self.base_luck = 10
+
+class Juggernaut(Character):
+	def config(self):
+		self.moves = []
+		self.elements = []
+		self.status = []
+		self.coefficients = (1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+		self.base_physical_strength = 15
+		self.base_physical_defense = 15
+		self.base_special_strength = 4
+		self.base_special_defense = 4
+		self.base_speed = 8
+		self.base_hp = 14
+		self.base_luck = 10
+
+class Battlemage(Character):
+	def config(self):
+		self.moves = []
+		self.elements = []
+		self.status = []
+		self.coefficients = (1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+		self.base_physical_strength = 8
+		self.base_physical_defense = 12
+		self.base_special_strength = 11
+		self.base_special_defense = 11
+		self.base_speed = 10
+		self.base_hp = 10
+		self.base_luck = 10
+
+class Nightblade(Character):
+	def config(self):
+		self.moves = []
+		self.elements = []
+		self.status = []
+		self.coefficients = (1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+		self.base_physical_strength = 12
+		self.base_physical_defense = 8
+		self.base_special_strength = 12
+		self.base_special_defense = 8
+		self.base_speed = 12
+		self.base_hp = 8
+		self.base_luck = 10
+
+class Spellsword(Character):
+	def config(self):
+		self.moves = []
+		self.elements = []
+		self.status = []
+		self.coefficients = (1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+		self.base_physical_strength = 12
+		self.base_physical_defense = 8
+		self.base_special_strength = 12
+		self.base_special_defense = 8
+		self.base_speed = 10
+		self.base_hp = 10
+		self.base_luck = 10
+
+class Witchhunter(Character):
+	def config(self):
+		self.moves = []
+		self.elements = []
+		self.status = []
+		self.coefficients = (1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+		self.base_physical_strength = 10
+		self.base_physical_defense = 8
+		self.base_special_strength = 8
+		self.base_special_defense = 13
+		self.base_speed = 11
+		self.base_hp = 10
+		self.base_luck = 10
 
