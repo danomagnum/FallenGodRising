56a3a2ee572160baf515ec3f4be640d3ea2754a9
dano <266759+danomagnum@users.noreply.github.com>
2020-04-05 14:57:20 +0000

    got bearlib workign with "tiles" for the map and regular text for other stuff. Still need to work on the bordering

---
+++ bearlibkeys.py
@@ -0,0 +1,13 @@
+from bearlibterminal import terminal
+
+UP = [terminal.TK_UP, terminal.TK_W]
+DOWN = [terminal.TK_DOWN, terminal.TK_S]
+LEFT = [terminal.TK_LEFT, terminal.TK_A]
+RIGHT = [terminal.TK_RIGHT, terminal.TK_D]
+SELECT = [terminal.TK_ENTER, terminal.TK_SPACE]
+BACK = [terminal.TK_BACKSPACE, terminal.TK_ESCAPE]
+DEBUG_H = [terminal.TK_H]
+DEBUG_J = [terminal.TK_J]
+DEBUG_K = [terminal.TK_K]
+DEBUG_L = [terminal.TK_L]
+EXIT = [terminal.TK_CLOSE]
