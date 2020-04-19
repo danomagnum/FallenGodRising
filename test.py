5c2b2282aa3c4d2bdbb04d059cd572a0aacc1cf5
dano <dano@5704632f-9846-42e2-9197-bb39f3c2fcad>
2020-04-19 18:27:50 +0000

    renamed test2.py to game.py.  This is more accurate at this point.

--- test.py
+++ test.py
@@ -1,105 +1,50 @@
-import sys
+import maps.bsp as bsp
+import maps.maptools as maptools
 
-sys.dont_write_bytecode = True
+SPLITS = 8
 
-import characters
-import battle
-import main
-import curses_interface as graphics_interface
-#import pygcurses_interface as graphics_interface
-import time
-import items
-import overworld
-import keys
-import entities
-from constants import *
+#XMAX = 60
+#YMAX = 36
+#
+#def map():
+#	return [['.' for x in range(XMAX)] for y in range(YMAX)]
+#
+#a = b.Tree(0, 0, XMAX - 1, YMAX - 1, 12, 2)
+#nosplit = map()
+#for leave in a.final_leaves():
+#	leave.draw(nosplit, True)
+#nosplit = mt.flatten(nosplit, True)
+#
+#a.split(True)
+#one_split = map()
+#for leave in a.final_leaves():
+#	print(leave.x0, leave.y0, leave.width, leave.height)
+#	leave.draw(one_split, True)
+#one_split = mt.flatten(one_split, True)
+#
+#a.split(True)
+#two_split = map()
+#for leave in a.final_leaves():
+#	print(leave.x0, leave.y0, leave.width, leave.height)
+#	leave.draw(two_split, True)
+#two_split = mt.flatten(two_split, True)
+#
+#a.split(True)
+#three_split = map()
+#for leave in a.final_leaves():
+#	print(leave.x0, leave.y0, leave.width, leave.height)
+#	leave.draw(three_split, True)
+#three_split = mt.flatten(three_split, True)
+#
+#
 
-import maps.testmap
+lev = [['.' for x in range(maptools.MAPSIZE[0])] for y in range(maptools.MAPSIZE[1])]
+buildings = bsp.Tree(0, 0, maptools.MAPSIZE[0] - 1, maptools.MAPSIZE[1] - 1, 8, 2)
 
-def dotestbattle(user, display, level=50):
+for split in range(SPLITS):
+	buildings.split(True)
 
-	sys.stdout.silent = True
-	battleenemy = characters.Page(level=(level+2))
-	battleenemy2 = characters.Cleric(level=(level-2))
-	battleenemy3 = characters.Fighter(level=(level-3))
+for building in buildings.final_nodes():
+	building.draw(lev, True)
 
-	enemy = battle.Random_AI([battleenemy,battleenemy2,battleenemy3])
-
-	sys.stdout.silent = False
-	display.flash_screen()
-	battle.Battle(user, enemy , display)
-
-
-try:
-	if __name__ == '__main__':
-		graphics_interface.initialize()
-		zone = maps.testmap.zone
-
-		user = characters.gen_testuser()
-		user.x, user.y = zone.find_empty_position()
-
-		display = graphics_interface.Display(zone=zone)
-		zone.display = display
-
-		zone.set_player(user)
-
-		display.user = user
-		loop = True
-
-		while loop:
-			key = display.mapbox.getch()
-			if key in keys.UP:
-				user.move(zone, UP)
-			elif key in keys.DOWN:
-				user.move(zone, DOWN)
-			elif key in keys.LEFT:
-				user.move(zone, LEFT)
-			elif key in keys.RIGHT:
-				user.move(zone, RIGHT)
-			elif key == ord('m'):
-				#Menu
-				choice = display.menu(['Battlers', 'Quests', 'Transport', 'Save', 'Options', 'Items'], 4)
-				if choice == 'Battlers':
-					display.mode=graphics_interface.STATS
-					display.refresh_full()
-					key = display.mapbox.getch()
-					display.mode=graphics_interface.MAP
-					display.refresh_full()
-				elif choice == 'Quests':
-					pass
-
-				elif choice == 'Items':
-					item_slot_used = display.menu(user.backpack.show(), cols=2)
-					if item_slot_used is not None:
-						item_target_type = item_slot_used.target_type
-						if item_target_type == SELF:
-							item_target = [display.menu(user.combatants, cols=2)]
-						elif item_target_type == MULTI_SELF:
-							item_target = user.combatants
-						elif item_target_type in EQUIPPABLE:
-							pass
-						else:
-							print("Can't Use that now")
-						if item_target is not None:
-							item_used = item_slot_used.take()
-							selection_needed = False
-							for t in item_target:
-								item_used.use(t)
-
-
-			elif key in keys.SELECT:
-				pass
-			zone.tick()
-			display.show_messages()
-			display.refresh_full()
-
-except Exception as e:
-	graphics_interface.shutdown()
-
-
-
-time.sleep(3)
-
-graphics_interface.shutdown()
-
-sys.exit(0)
+maptools.flatten(lev, True)
