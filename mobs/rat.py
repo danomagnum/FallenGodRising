9324b750c7d25f63cf749b0239aa5ee2cf4a812a
dano <266759+danomagnum@users.noreply.github.com>
2020-06-30 23:25:15 +0000

    added mob

---
+++ mobs/rat.py
@@ -0,0 +1,40 @@
+from .characters import Character, promote
+import moves
+import elements
+import utility
+import random
+
+
+class LittleRat(Character):
+	def config(self):
+		self.moves = [moves.Strike(self.game), moves.Buff(self.game)]
+		self.base_stats()
+	def base_stats(self):
+		self.base_hp = 30
+		self.base_physical_strength = 56 
+		self.base_physical_defense = 35
+		self.base_special_strength = 25
+		self.base_special_defense = 35
+		self.base_speed = 72
+		self.base_luck = 100
+
+	def level_20(self):
+		promote(self, Rat)	
+
+
+class Rat(Character):
+	def config(self):
+		self.moves = [moves.Strike(self.game), moves.Buff(self.game)]
+		self.base_stats()
+	def base_stats(self):
+		self.base_hp = 55
+		self.base_physical_strength = 81 
+		self.base_physical_defense = 60
+		self.base_special_strength = 50
+		self.base_special_defense = 70
+		self.base_speed = 97
+		self.base_luck = 100
+
+
+
+trash = [LittleRat]
