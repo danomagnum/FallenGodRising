477afdfc8f30ad1fb0f21743924e1d156b2df9be
dano <266759+danomagnum@users.noreply.github.com>
2020-10-18 17:00:50 +0000

    version number not updating

--- version.py
+++ version.py
@@ -1,3 +1,3 @@
 version = "0.1.1"
-revision = ''.join(filter(str.isdigit, "$Revision$"))
+revision = ''.join(filter(str.isdigit, "$Revision: 144$"))
 versionstring = 'Version: {} / Revision: {} '.format(version, revision)
