ca44f0b7bafca051da9cbbb4462e1770f33ae8f2
dano <dano@5704632f-9846-42e2-9197-bb39f3c2fcad>
2020-10-18 17:01:33 +0000

    same

--- version.py
+++ version.py
@@ -1,3 +1,3 @@
 version = "0.1.1"
-revision = ''.join(filter(str.isdigit, "$Revision: 144$"))
+revision = ''.join(filter(str.isdigit, "$Revision$"))
 versionstring = 'Version: {} / Revision: {} '.format(version, revision)
