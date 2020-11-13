481159fe649edf77ed93746ba500aaca0d2cddac
dano <dano@5704632f-9846-42e2-9197-bb39f3c2fcad>
2020-11-13 21:56:49 +0000

    added other boosters and changed them to elements instead of books so books can be used for storytelling

---
+++ items/boosts.py
@@ -0,0 +1,115 @@
+from constants import *
+from .item import Item
+
+class ExpBoost(Item):
+	def config(self):
+		self.name = 'Element of Knowledge'
+		self.weigth = 1
+		self.target_type = ANY
+		self.value = 100
+		self.rarity = 0.01
+		self.helptext = 'Gain Some EXP.'
+		self.char = '\x0E'
+
+	def use(self, target):
+		exp0 = target.exp_at_level(target.level)
+		exp1 = target.exp_at_level(target.level + 1)
+		exp_delta = exp1 - exp0 + 1
+		target.exp += exp_delta
+		print('{} used {} and gained {} exp.'.format(target.name,self.name, exp_delta))
+
+
+class StrBoost(Item):
+	def config(self):
+		self.name = 'Element of Strength'
+		self.weigth = 1
+		self.target_type = ANY
+		self.value = 100
+		self.rarity = 0.01
+		self.helptext = 'Permanently Boost Strength'
+		self.char = '\x0E'
+
+	def use(self, target):
+		gain = random.randint(1, 5)
+		target.base_physical_strength += gain
+		print('{} used {} and gained {} physical str.'.format(target.name,self.name, gain))
+		
+class DefBoost(Item):
+	def config(self):
+		self.name = 'Element of Defense'
+		self.weigth = 1
+		self.target_type = ANY
+		self.value = 100
+		self.rarity = 0.01
+		self.helptext = 'Permanently Boost Defense'
+		self.char = '\x0E'
+
+	def use(self, target):
+		gain = random.randint(1, 5)
+		target.base_physical_defense += gain
+		print('{} used {} and gained {} physical def.'.format(target.name,self.name, gain))
+	
+class ArcStrBoost(Item):
+	def config(self):
+		self.name = 'Element of Impulse'
+		self.weigth = 1
+		self.target_type = ANY
+		self.value = 100
+		self.rarity = 0.01
+		self.helptext = 'Permanently Boost Arcane Strength'
+		self.char = '\x0E'
+
+	def use(self, target):
+		gain = random.randint(1, 5)
+		target.base_arcane_strength += gain
+		print('{} used {} and gained {} arcane str.'.format(target.name,self.name, gain))
+	
+class ArcDefBoost(Item):
+	def config(self):
+		self.name = 'Element of Fortitude'
+		self.weigth = 1
+		self.target_type = ANY
+		self.value = 100
+		self.rarity = 0.01
+		self.helptext = 'Permanently Boost Arcane Defense'
+		self.char = '\x0E'
+
+	def use(self, target):
+		gain = random.randint(1, 5)
+		target.base_arcane_efense += gain
+		print('{} used {} and gained {} arcane def.'.format(target.name,self.name, gain))
+
+class SpdBoost(Item):
+	def config(self):
+		self.name = 'Element of Time'
+		self.weigth = 1
+		self.target_type = ANY
+		self.value = 100
+		self.rarity = 0.01
+		self.helptext = 'Permanently Boost Speed'
+		self.char = '\x0E'
+
+	def use(self, target):
+		gain = random.randint(1, 5)
+		target.base_speed += gain
+		print('{} used {} and gained {} speed'.format(target.name,self.name, gain))
+
+class LuckBoost(Item):
+	def config(self):
+		self.name = 'Element of Chance'
+		self.weigth = 1
+		self.target_type = ANY
+		self.value = 100
+		self.rarity = 0.01
+		self.helptext = 'Permanently Boost Luck'
+		self.char = '\x0E'
+
+	def use(self, target):
+		gain = random.randint(1, 5)
+		target.base_luck += gain
+		print('{} used {} and gained {} luck'.format(target.name,self.name, gain))
+
+
+
+
+base_items = [ExpBoost, StrBoost, DefBoost, ArcStrBoost, ArcDefBoost, SpdBoost, LuckBoost]
